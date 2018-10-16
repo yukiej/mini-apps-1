c80fd43ce57299e2132f868ed84a364ecbc970e0
Yuki Jung <yukiej@gmail.com>
2018-10-16 12:11:30 -0700

    Made express server to serve static assets in client folder

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,10 @@
+var express = require('express');
+var app = express();
+const port = 5000;
+
+app.use(express.static('client'));
+
+app.listen(port, () => console.log(`JSON conversion app listening on port ${port}!`));
+
 
 
