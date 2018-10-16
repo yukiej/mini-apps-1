c62cb6c6645d382aa6368e0489b7f0aad3a0c418
Yuki Jung <yukiej@gmail.com>
2018-10-16 13:47:18 -0700

    Get out the JSON input on submit

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+
+app.use(bodyParser.json());
 app.use(express.static('client'));
+
+app.post('/', function (req, res) {
+  let inputJSON = req.body.json_name;
+  res.send('Here is the req!');
+  console.log(inputJSON);
+})
 
 app.listen(port, () => console.log(`JSON conversion app listening on port ${port}!`));
 
