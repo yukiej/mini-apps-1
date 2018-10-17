330cee6c576b760f1321a7e123ebfa13640cf9e0
Yuki Jung <yukiej@gmail.com>
2018-10-17 14:02:11 -0700

    MongoDB database talks to the server
    Added Mongoose

--- challenge_3/database.js
+++ challenge_3/database.js
@@ -1,13 +1,3 @@
-//Set up connection to database
-var MongoClient = require('mongodb').MongoClient
-var Server = require('mongodb').Server;
+use ShopDB;
 
-//Set up connection to the local server?
-var mongoClient = new MongoClient(new Server('localhost', 4000));
-
-//Make connection
-mongoClient.connect("mongodb://localhost:4000/ShopDB", function(err, db) {
-  if(!err) {
-    console.log("Connection made!");
-  }
- })
+db.transactCol.find():
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,18 +1,44 @@
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 4000;
+const router = express.Router();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'))
 
 //Set up connection to database?
 const MongoClient = require('mongodb').MongoClient
 //do I need this?
-// const Server = require('mongodb').Server;
+const Server = require('mongodb').Server;
+
+
+
+app.listen(port, function() {
+  console.log('listening on ', port);
+})
+
+
 
 //Set up connection to the local server?
 // const mongoClient = new MongoClient(new Server('localhost', port));
 
 //Make connection
-MongoClient.connect(`mongodb://localhost:${port}/ShopDB`,  { useNewUrlParser: true }, function(err, db) {
-  if(!err) {
-    console.log("Connection made!");
+MongoClient.connect(`mongodb://localhost/ShopDB`,  { useNewUrlParser: true }, function(err, db) {
+  if (err) {
+    throw err;
   }
+    let currentDB = db.db('ShopDB');
+
+    currentDB.collection('transactCol').find().toArray(function(err, result) {
+      if (err) {
+        throw err
+      } else {
+        console.log(result);
+      }
+    })
+    // console.log("Connection made to: ", db);
  })
+
